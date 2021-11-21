7e28814ab4244bd69573daa109d9a0d6316a5520
JahnaviPS <69338685+JahnaviPS@users.noreply.github.com>
2021-11-21 12:09:13 +0530

    Add files via upload

---
+++ EmpProjectController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HRBAL1;
+using HRDAL;
+using System.Web.Http.Cors;
+
+namespace HRServiceProject.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class EmpProjectController : ApiController
+    {
+        [HttpGet]
+        public List<EmpProject> showdetails()
+        {
+            EmpProjectDAL d = new EmpProjectDAL();
+            List<EmpProject> f = d.showdetails();
+            return f;
+
+
+        }
+    }
+}
