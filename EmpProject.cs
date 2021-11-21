1f37d55c54501464d33563fd272e338f02e11672
JahnaviPS <69338685+JahnaviPS@users.noreply.github.com>
2021-11-21 12:07:53 +0530

    Add files via upload

---
+++ EmpProject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRBAL1
+{
+   public class EmpProject
+    {
+      
+            
+       public int EmpId { get; set; }
+
+       public int ProjId { get; set; }
+    }
+}
